cda556b80bb7b9d7c31e0dcb9916d09ae6c5c7ad
yashwanth <yashwanth@Machine.local>
2016-12-05 01:02:15 -0800

    first commit

---
+++ ANSI-Solutions/GoogleFeedsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  GoogleFeedsViewController.swift
+//  ANSI-Solutions
+//
+//  Created by yashwanth on 12/4/16.
+//  Copyright © 2016 srikanth. All rights reserved.
+//
+
+import Foundation
---
+++ ANSI-Solutions/TableViewCell.swift
@@ -0,0 +1,9 @@
+//
+//  TableViewCell.swift
+//  ANSI-Solutions
+//
+//  Created by yashwanth on 12/4/16.
+//  Copyright © 2016 srikanth. All rights reserved.
+//
+
+import Foundation
